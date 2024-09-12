1e0a29b9ef80daaf7adec253ce75cf6e1413ecf8
vanishaar <61094116+vanishaar@users.noreply.github.com>
2024-09-12 20:25:34 +0530

    Add files via upload

---
+++ arithmetic.py
@@ -0,0 +1,9 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    # print sum of a and b
+    print(a+b)
+    # print  difference of a and b
+    print(a-b)
+    #print product of a and b
+    print(a*b)
---
+++ printfunction.py
@@ -0,0 +1,8 @@
+if __name__ == '__main__':
+    n = int(input())
+    string = ""
+    i = 1
+    while i <= n:
+        string = string + str(i)
+        i +=1
+    print(string)
